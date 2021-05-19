2d0676414acf3f87611d989c922cd9612b39d0d4
isjuyeon <joosu2454@daum.net>
2021-05-19 21:06:18 +0900

    by 2

--- Airplane_crawling.py
+++ Airplane_crawling.py
@@ -17,3 +17,11 @@ browser.find_elements_by_link_text("27")[0].click()
 browser.find_elements_by_link_text("28")[1].click()
 
 browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]").click()
+
+
+browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]").click()
+
+browser.find_element_by_link_text("항공권 검색").click()
+elem = webdriverwait(browser,10)/until(EC.presence_of_element_located((By.XPATH,"//*[@id='conten']/div[2]/div/div[4]/ul/li[1]")))
+elem=browser.find_element_by_xpath("//*[@id='conten']/div[2]/div/div[4]/ul/li[1]")
+print(elem.text)
