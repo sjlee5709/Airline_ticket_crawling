ccf9fc9e2df88aa14e87cf9c2c8606be72e9a3bf
isjuyeon <joosu2454@daum.net>
2021-05-19 23:07:47 +0900

    second by2

--- Airplane_crawling1.py
+++ Airplane_crawling1.py
@@ -15,3 +15,11 @@ browser.find_elements_by_link_text("27")[0].click()
 browser.find_elements_by_link_text("28")[1].click()
 
 browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]").click()
+
+browser.find_element_by_link_text("항공권 검색").click()
+
+try:
+    elem = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul/li[1]")))
+    print(elem.text)
+finally:
+    browser.quit()
